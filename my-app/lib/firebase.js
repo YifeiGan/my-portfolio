2c31f392fd22981a24a7d2fc453cca07db7bcbe5
Yifei Gan <stellagyf0919@163.com>
2026-03-26 00:11:51 -0700

    fix api problem

--- my-app/lib/firebase.js
+++ my-app/lib/firebase.js
@@ -5,13 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // ⚠️ 这里替换成你自己在 Firebase Console -> 项目设置里看到的配置
 const firebaseConfig = {
-    apiKey: "AIzaSyBrjcHBw9FcgH5F1mkIRqjLMB3DlomiakQ",
-    authDomain: "yifei-portfolio-fbed4.firebaseapp.com",
-    projectId: "yifei-portfolio-fbed4",
-    storageBucket: "yifei-portfolio-fbed4.firebasestorage.app",
-    messagingSenderId: "351339950661",
-    appId: "1:351339950661:web:bea27f04e6c1d3ff89a92c",
-    measurementId: "G-1VMWKSG567"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // 防止 Next.js 热重载时重复初始化 Firebase
